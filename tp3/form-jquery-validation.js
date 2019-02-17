33f32f233a242ec30e30233deea9d1c68e9cda34
Hamzaaouad <44164380+Hamzaaouad@users.noreply.github.com>
2019-02-17 18:04:31 +0100

    Update form-jquery-validation.js

--- tp3/form-jquery-validation.js
+++ tp3/form-jquery-validation.js
@@ -1,109 +1,71 @@
+$( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+      
 
-$(document).ready(function() {
-
-    console.log( "DOM ready!" );
-
-                
-    
-        $("#envoyer").on("click", function (event) {
-
-            event.preventDefault();
             $(document).keyup(function(){
 
-
-                if($("#nom").val().length == 0 ){
-                    $("#nom").css("border-color","red");
-                    $(".modal-title").text(" Attention : erreur de saisie ");
-                    $(".modal-body").html("Le champs nom est vide. Veuillez le remplir !");
-                    $("#myModal").modal("show");
-
+                valid = true ;
+                if($("#nom").val().length < 5 ){
+                    $("#nom").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#nom").css("border-color","#48DE14");
+                }
+                if($("#Prenom").val().length < 5 ){
+                    $("#Prenom").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#Prenom").css("border-color","#48DE14");
+                }
+                if($("#datedenaissance").val().length < 5 ){
+                    $("#datedenaissance").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#datedenaissance").css("border-color","#48DE14");
+                }
+                if($("#Adresse").val().length < 5 ){
+                    $("#Adresse").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#Adresse").css("border-color","#48DE14");
+                }
+                if($("#email").val().length < 5 ){
+                    $("#email").css("border-color","#ff0000");
+                    valid=false;
+                }
+                else{
+                    $("#email").css("border-color","#48DE14");
                 }
 
-                else 
-                    if($("#nom").val().length < 5 ){
-                    $("#nom").css("border-color","red");
-                    $(".modal-title").text(" Attention : erreur de saisie ");
-                    $(".modal-body").html("Le champs nom doit contenir au moins 5 caractères !");
-                    $("#myModal").modal("show");    
-                    }
-                    else
-                    if($("#Prenom").val().length < 5 ){
-                        $("#Prenom").css("border-color","red");
-                        $(".modal-title").text(" Attention : erreur de saisie ");
-                        $(".modal-body").html("Le champs Prénom doit contenir au moins 5 caractères !");
-                        $("#myModal").modal("show");
-                        }
-                        
-    
-                        else 
-                            if($("#Prenom").val().length == 0 ){
-                            $("#Prenom").css("border-color","red");
-                            $(".modal-title").text(" Attention : erreur de saisie ");
-                            $(".modal-body").html("Le champs Prénom est vide. Veuillez le remplir !");
-                            $("#myModal").modal("show");
-                            }
-                            else
-                                if($("#datedenaissance").val().length<5){
-                                    $("#datedenaissance").css("border-color","red");
-                                    $(".modal-title").text(" Attention : erreur de saisie ");
-                                    $(".modal-body").html("Le champs Date de naissance doit contenir au moins 5 caractères !");
-                                    $("#myModal").modal("show");
-                                    }
-                                    else
-                                        if($("#datedenaissance").val().length == 0 ){
-                                        $("#datedenaissance").css("border-color","red");
-                                        $(".modal-title").text(" Attention : erreur de saisie ");
-                                        $(".modal-body").html("Le champs Date de naissance est vide. Veuillez le remplir !");
-                                        $("#myModal").modal("show");
-                                        }
-                                        else
-                                            if($("#Adresse").val().length == 0 ){
-                                                $("#Adresse").css("border-color","red");
-                                                $(".modal-title").text(" Attention : erreur de saisie ");
-                                                $(".modal-body").html("Le champs Adresse est vide. Veuillez le remplir !");
-                                                $("#myModal").modal("show");
-                                                }
-                            
-                                                else 
-                                                    if($("#Adresse").val().length < 5 ){
-                                                    $("#Adresse").css("border-color","red");
-                                                    $(".modal-title").text(" Attention : erreur de saisie ");
-                                                    $(".modal-body").html("Le champs Adresse doit contenir au moins 5 caractères !");
-                                                    $("#myModal").modal("show");
-                                                    }
-                                                    else
-                                                        if($("#email").val().length == 0 ){
-                                                            $("#email").css("border-color","red");
-                                                            $(".modal-title").text(" Attention : erreur de saisie ");
-                                                            $(".modal-body").html("Le champs Email est vide. Veuillez le remplir !");
-                                                            $("#myModal").modal("show");
-                                                            }
-                                        
-                                                            else 
-                                                                if($("#email").val().length < 5 ){
-                                                                $("#email").css("border-color","red");
-                                                                $(".modal-title").text(" Attention : erreur de saisie ");
-                                                                $(".modal-body").html("Le champs email doit contenir au moins 5 caractères !");
-                                                                $("#myModal").modal("show");
-                                                                }
-                                                                else
-                                                                {
-                                                                    $("#nom").css("border-color","green");
-                                                                    $("#Prenom").css("border-color","green");
-                                                                    $("#datedenaissance").css("border-color","green");
-                                                                    $("#Adresse").css("border-color","green");
-                                                                    $("#email").css("border-color","green");
-                                                                    $(".modal-title").html("Bienvenue "+ $("#Prenom").val()+" "+$("#nom").val())+"<br/>"+" Vous êtes né en : "+$("#datedenaissance").val()+"<br/>";
-                                                                    $(".error").text("");
-                                                                    $(".modal-body").append('<img src = "https://maps.googleapis.com/maps/api/staticmap?markers=' + $("#Adresse").val() + '&zoom=12&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"><br>');
-                                                                    $('#myModal').modal("show");
-                                                                }
-                    return valid;
-                });
-            });
-            
+                return valid;
 
+            });
+
+
+            $("#envoyer").on("click", function (event) {
+            event.preventDefault();
+        
+    
+         if($("#nom").val() !== "" && $("#Prenom").val() !== "" && $("#datedenaissance").val() !== "" 
+         && $("#Adresse").val() !== "" && $("#email").val() !== "" ){
             
+            $('#myModal').modal("show");
+            $(".modal-title").html("Bienvenue "+ document.querySelector("#Prenom").value);
+             $('.modal-body').html("Vous étes né le: "+ document.querySelector("#datedenaissance").value+
+            "</br>Et vous Habitez à: "+ document.querySelector("#Adresse").value
+            +'</br><a href="https://maps.google.com/maps?q='
+                                +document.querySelector("#Adresse").value
+                                +'"><img src="https://maps.googleapis.com/maps/api/staticmap?markers='
+                                +document.querySelector("#Adresse").value+'&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/></a>');
+        }
+        
+    });
     
 
 });
